17be6ae06c67cbd48bdf2b0f8c93bb59de3cb76c
Kurt Weinheimer <kurtweinheimer@Kurts-MacBook-Pro.local>
2023-11-08 09:58:11 -0500

    grid circles

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -5,24 +5,48 @@ type Props = {}
 const AboutUs = (props: Props) => {
   return (
     <div className='w-full h-fit p-[10vw] flex flex-col bg-my-baby-purple font-mukta-mahee text-my-midnight-blue'>
-        <div className="lg:w-[20vw] items-center justify-center z-20">
-            <div className=" relative pb-[20%] flex items-center justify-center">
-                <div className="absolute top-0 w-full h-full items-center flex justify-center">
-                    <div className="absolute z-30 w-full h-full mix-blend-lighten">
-                        <video
-                            id='about-us-vid'
-                            autoPlay
-                            loop
-                            muted
-                            playsInline
-                            className="w-full h-full object-cover"
-                        >
-                            <source src="/animations/about-us-typed.mp4" type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
+        <div className='flex items-center'>
+            <div className="lg:w-[20vw] items-center justify-center z-20">
+                <div className=" relative pb-[20%] flex items-center justify-center">
+                    <div className="absolute top-0 w-full h-full items-center flex justify-center">
+                        <div className="absolute z-30 w-full h-full mix-blend-lighten">
+                            <video
+                                id='about-us-vid'
+                                autoPlay
+                                loop
+                                muted
+                                playsInline
+                                className="w-full h-full object-cover"
+                            >
+                                <source src="/animations/about-us-typed.mp4" type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
                     </div>
+                </div>                     
+            </div>
+
+            <div className='ml-auto mr-[5vw]'>
+                <div className="lg:w-[20vw] items-center justify-center z-20">
+                    <div className=" relative pb-[100%] flex items-center justify-center">
+                        <div className="absolute top-0 w-full h-full items-center flex justify-center">
+                            <div className="absolute z-30 w-full h-full">
+                                <video
+                                    id='about-us-vid'
+                                    autoPlay
+                                    loop
+                                    muted
+                                    playsInline
+                                    className="w-full h-full object-cover"
+                                >
+                                    <source src="/animations/grid-circles.mp4" type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        </div>
+                    </div>                     
                 </div>
-            </div>                     
+            </div>
         </div>
 
         <div className=' flex text-3xl mt-[50px]'>
